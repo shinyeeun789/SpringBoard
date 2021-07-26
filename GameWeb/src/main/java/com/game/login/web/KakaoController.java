a1f276e2663bab9c0ca5afd0d7808f870ab183c3
hee9841 <lee98417716@gmail.com>
2021-07-26 15:23:39 +0900

    토큰 값, 사용자 정보 가져오기

--- GameWeb/src/main/java/com/game/login/web/KakaoController.java
+++ GameWeb/src/main/java/com/game/login/web/KakaoController.java
@@ -1,5 +1,7 @@
 package com.game.login.web;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,12 +9,31 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.apache.tomcat.util.bcel.classfile.ElementValuePair;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+
+
 @Controller
-public class KakaoController {
+public class KakaoController  {
 
 	private final static String K_CLIENT_ID = "db1cd9e8343b76d2a21deae2ce17abbd";
-	private final static String K_REDIRECT_URI="http://localhost:8081/";
+	private final static String K_REDIRECT_URI="http://localhost:8081/main.do";
 	
+	/** authorization Url get
+	 * 
+	 * @param session
+	 * @return
+	 */
 	public String getAuthorizzationUrl(HttpSession session) {
 		
 		String kakoUrl = "https://kauth.kako.com/oauth/authorize?"
@@ -21,10 +42,79 @@ public class KakaoController {
 		return null;
 	}
 	
-	public String getAccessToken(String autorize_code) {
+	
+	/** access토큰 get
+	 * 
+	 * @param autorize_code
+	 * @return
+	 * @throws ClientProtocolException
+	 * @throws IOException
+	 */
+	public String getAccessToken(String autorize_code) throws ClientProtocolException, IOException {
 		
 		final String RequestUrl = "https://kauth.kakao.com/oauth/token";
-		//final List<NameValuePair> postParams = new ArrayList<NameValuePair>();
-		return null;
+		final List<NameValuePair> postParams = new ArrayList<NameValuePair>();
+		postParams.add(new BasicNameValuePair("grant_type", "authorization_code"));
+		postParams.add(new BasicNameValuePair("client_id", K_CLIENT_ID)); //REST API KEY
+		postParams.add(new BasicNameValuePair("redirect_uri", K_REDIRECT_URI)); // redrect uri
+		postParams.add(new BasicNameValuePair("code", autorize_code)); // 로그인 과정중 얻는 code 값
+		
+		final HttpClient client = HttpClientBuilder.create().build();
+		final HttpPost post = new HttpPost(RequestUrl);
+		JsonNode returnNode=null;
+		
+		try {
+			post.setEntity(new UrlEncodedFormEntity(postParams));
+			final HttpResponse response = client.execute(post);
+			final int reposeCode =response.getStatusLine().getStatusCode();
+			
+			//JSON 형태 반환값 처리
+			
+			ObjectMapper mapper = new ObjectMapper();
+			returnNode = mapper.readTree(response.getEntity().getContent());
+			
+		}catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}catch (IOException e){
+			e.printStackTrace();
+		}
+		
+		return returnNode.get("access_token").toString();
+	}
+	
+	public JsonNode getKakaoUserInfo(String autorize_code) throws ClientProtocolException, IOException {
+		
+		final String RequestUrl = "https://kapi.kakao.com/v1/user/me";
+		
+		final HttpClient client = HttpClientBuilder.create().build();
+		final HttpPost post = new HttpPost(RequestUrl);
+		String accessToken = getAccessToken(autorize_code);
+		
+		post.addHeader("Authorization", "Bearer " + accessToken);
+		
+		JsonNode returnNode =null;
+		
+		try {
+			
+			final HttpResponse response = client.execute(post);
+			final int responseCode = response.getStatusLine().getStatusCode();
+			System.out.println("\nSending 'POST request to URL : " + RequestUrl);
+			System.out.println("Response Code : " + responseCode);
+			
+			//Json 형태 반환값 처리
+			ObjectMapper mapper = new ObjectMapper();
+			returnNode = mapper.readTree(response.getEntity().getContent());
+			
+		}catch (UnsupportedEncodingException e){
+			e.printStackTrace();
+		}catch (ClientProtocolException e) {
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		return returnNode;
 	}
 }
