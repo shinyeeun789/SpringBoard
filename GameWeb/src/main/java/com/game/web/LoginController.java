cfc227a526a837884af7b95d38a240cf2d7cbfbc
shinyeeun789 <70800414+shinyeeun789@users.noreply.github.com>
2021-07-28 09:12:44 +0900

    Update LoginController.java

--- GameWeb/src/main/java/com/game/web/LoginController.java
+++ GameWeb/src/main/java/com/game/web/LoginController.java
@@ -34,13 +34,7 @@ public class LoginController {
 	private LoginService service;
 	
 	@Inject
-<<<<<<< HEAD
 	BCryptPasswordEncoder pwdEncoder;		// 암호화 기능
-
-=======
-	BCryptPasswordEncoder pwdEncoder;
-	
->>>>>>> 7c15d7493ae9890201f76d7f5e7526589f404c52
 	private KakaoRestApi kakao_rest_api = new KakaoRestApi();
 	
 	
@@ -48,11 +42,6 @@ public class LoginController {
 	public void login(Model model, HttpSession session) throws Exception {
 		
 		String KakaoUrl = kakao_rest_api.getAuthorizationUrl(session);
-<<<<<<< HEAD
-		
-=======
-		//ìƒì„±í•œ ì¸ì¦ URLì„ Viewë¡œ ì „ë‹¬
->>>>>>> 7c15d7493ae9890201f76d7f5e7526589f404c52
 		model.addAttribute("kakao_url", KakaoUrl);
 		
 		System.out.println("/login/login");
@@ -94,12 +83,8 @@ public class LoginController {
 	@RequestMapping(value = "/signUp.do", method = RequestMethod.POST)
 	public String signUpPOST(Map<String, Object> modelMap, LoginVO userInfo, HttpServletRequest request) throws Exception {
 		logger.info("post signUp");
-<<<<<<< HEAD
 		
 		// 비밀번호 암호화하여 userInfo에 넣어주기
-=======
-
->>>>>>> 7c15d7493ae9890201f76d7f5e7526589f404c52
 		String pwd = pwdEncoder.encode(userInfo.getUserPW());
 		userInfo.setUserPW(pwd);
 		
@@ -111,10 +96,6 @@ public class LoginController {
 		return "/login/signUp.do";
 	}
 	
-<<<<<<< HEAD
-=======
-	// ID ì¤‘ë³µ í™•ì¸
->>>>>>> 7c15d7493ae9890201f76d7f5e7526589f404c52
 	@RequestMapping(value = "/idCheck.do", method = RequestMethod.POST)
 	public void idCheck(HttpServletRequest request, String userID, HttpServletResponse response) throws Exception {
 		JSONObject jsonObject = new JSONObject();
